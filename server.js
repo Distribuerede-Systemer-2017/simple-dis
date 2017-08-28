d46e8a648c7f37d8cee9fab4392d8e1c20fcd09b
Jesper Bruun Hansen <jesperbbhansen@gmail.com>
2017-08-28 20:25:20 +0200

    sockets

--- server.js
+++ server.js
@@ -2,16 +2,34 @@ const express = require("express");
 const app = express();
 const http = require('http').Server(app);
 const port = process.env.PORT || 3000;
+const io = require("socket.io")(http);
 
+let counter = 0;
 
+//Endpoints
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/count', (req, res) => {
-  res.send({succes:true});
+  res.send({count:counter});
 });
 
+//Sockets
+io.on('connection', (client) => {
+
+  console.log("Client connected");
+
+  client.on('count', (data) => {
+    counter++;
+    io.sockets.emit('count', counter);
+  });
+  client.on('disconnect', () =>{
+    console.log("Client disconnected");
+  });
+});
+
+//Server
 http.listen(port, () => {
   console.log('listening on *:' + port);
 });
